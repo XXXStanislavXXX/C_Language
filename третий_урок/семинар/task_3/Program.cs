26c0bea3d525156c75d265971fa884f871f84841
Stanislav <stasperfiliyev@gmail.com>
2023-02-03 01:16:09 +0600

    семинар_3

---
+++ третий_урок/семинар/task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21
+*/
+
+System.Console.WriteLine("enter coordinates point xA ");
+int xA = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("enter coordinates point xB");
+int xB = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("enter coordinates point yA");
+int yA = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("enter coordinates point yB");
+int yB = int.Parse(Console.ReadLine());
+
+// System.Console.WriteLine($"{Math.Sqrt((xB - xA) * (xB - xA) + (yB - yA) * (yB - yA))}");
+
+double result =  Math.Round((Math.Sqrt((xB - xA) * (xB - xA) + (yB - yA) * (yB - yA))), 2);
+System.Console.WriteLine(result);
+
---
+++ третий_урок/семинар/task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел 
+от 1 до N.
+*/
+
+System.Console.WriteLine("enter nubler ");
+int digit = int.Parse(Console.ReadLine());
+int start = 1;
+while(start <= digit )
+{
+    double result = Math.Pow(start, 3 );
+    start ++ ;
+    System.Console.Write(result + ", ");
+}
