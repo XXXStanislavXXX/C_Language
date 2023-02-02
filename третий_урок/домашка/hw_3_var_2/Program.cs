9d5622796a124f9dacb4242fe7197c3f2d435f67
Stanislav <stasperfiliyev@gmail.com>
2023-02-03 02:05:05 +0600

    домашка с палиндромом вариант 2

---
+++ третий_урок/домашка/hw_3_var_2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+System.Console.WriteLine("Enter your number ");
+int num = int.Parse(Console.ReadLine());
+int num1 = num;
+int rev_num = 0;
+while (num > 0) 
+{
+    rev_num = rev_num * 10 + num % 10;
+    num = num / 10;
+}
+
+if (num1 == rev_num)
+{
+    System.Console.WriteLine("Your number  is a palindrome");
+}
+else
+{
+    System.Console.WriteLine("Your number is not a palindrome");
+}
