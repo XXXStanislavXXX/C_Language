b9014a3abd3f445790e319bb53a3ca58e8ece982
Stanislav <stasperfiliyev@gmail.com>
2023-02-10 13:34:33 +0600

    начало домашки

--- 5_урок/семинар/sample_1/Program.cs
+++ 5_урок/семинар/sample_1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные 
+и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+*/
+
+#nullable disable
+
+int [] Array = GetArray(4, -8, 8);
+Console.WriteLine(String.Join(" ", Array));
+int [] GetArray(int size, int minimum, int maximum)
+{
+    int [] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minimum, maximum + 1);
+    } 
+
+    return res;
+}
+
+for (int i = 0 ; i < Array.Length; i++)
+{
+    Array[i] *= -1;
+}
+Console.WriteLine(String.Join(" ", Array));
