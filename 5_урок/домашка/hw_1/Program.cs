d9e502f4604aca97063522864c47290814d5f62a
Stanislav <stasperfiliyev@gmail.com>
2023-02-12 00:04:42 +0600

    HW1,2

--- 5_урок/домашка/hw_1/Program.cs
+++ 5_урок/домашка/hw_1/Program.cs
@@ -6,3 +6,42 @@
 
 #nullable disable
 
+System.Console.WriteLine("Enter array legth: ");
+int length = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Enter array minimal: ");
+int min = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Enter array maximal: ");
+int max = int.Parse(Console.ReadLine());
+
+int[] Array = GetArray(length, min, max);
+
+System.Console.WriteLine($"[{string.Join(", ", Array)}]");
+
+FindEven(Array);
+
+int[] GetArray (int size, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] result = new int[size];
+    for(int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue+ 1);        
+    }
+    return result;
+}
+
+void FindEven(int[] MyArray)
+{
+    int even=0;
+    for (int i= 0; i< MyArray.Length; i++)
+    {
+        if (MyArray [i] %2 == 0)
+        {
+            even += 1;
+        }    
+    }
+    
+    System.Console.WriteLine($"even digits number: {even}");
+}
--- 5_урок/домашка/hw_2/Program.cs
+++ 5_урок/домашка/hw_2/Program.cs
@@ -7,3 +7,41 @@
 */
 
 #nullable disable
+
+System.Console.WriteLine("Enter array legth: ");
+int length=int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Enter array minimal: ");
+int min=int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Enter array maximal: ");
+int max=int.Parse(Console.ReadLine()!);
+
+int[] Array= GetArray(length, min, max);
+
+System.Console.WriteLine($"[{string.Join(", ", Array)}]");
+
+FindOddness (Array);
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    
+    Random rnd= new Random();
+    int [] result= new int[size];
+    for (int i= 0; i< result.Length; i++)
+    {
+        result [i] = rnd.Next(minValue, maxValue+ 1);        
+    }
+    return result;
+}
+
+void FindOddness (int[] MyArray)
+{
+    int odd = 0;
+    for(int i = 1; i < MyArray.Length; i=i+2)
+    {
+        odd = odd + MyArray[i];
+    }
+
+    System.Console.WriteLine($"Oddness summ = {odd}");
+}
