018c47d133b05782720abeac3e3ad1b414134eaf
Stanislav <stasperfiliyev@gmail.com>
2023-02-12 00:29:02 +0600

    ljvfirf 3

--- 5_урок/домашка/hw_3/Program.cs
+++ 5_урок/домашка/hw_3/Program.cs
@@ -6,3 +6,47 @@
 
 #nullable disable
 
+Console.WriteLine("Enter array legth  ");
+int Length = int.Parse(Console.ReadLine());
+
+double[] digits = new double[Length];
+
+FillArrayRandomDigits(digits);
+Console.WriteLine("Your Array: ");
+
+PrintArray(digits);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < digits.Length; i++)
+{
+    if (digits[i] > max)
+        {
+            max = digits[i];
+        }
+    if (digits[i] < min)
+        {
+            min = digits[i];
+        }
+}
+
+Console.WriteLine($"Your array length is: {digits.Length}. Your max number is = {max}, your minimal number is = {min}");
+Console.WriteLine($"Yur max, min difference is = {max - min}");
+
+void FillArrayRandomDigits(double[] digits)
+{
+    for(int i = 0; i < digits.Length; i++)
+        {
+            digits[i] = Convert.ToDouble(new Random().Next(100,100000)) / 100;
+        }
+}
+void PrintArray(double[] digits)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < digits.Length; i++)
+        {
+            Console.Write(digits[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
